d350e69391af5fec8d5d583fcd7a19e40f22236c
pkar <pkar@ieee.org>
2023-09-24 13:34:51 +0200

    chore: WinOCR is from Win7, not only Win10

--- Auto_n6_WinOCR/Auto_WinFace.vb
+++ Auto_n6_WinOCR/Auto_WinFace.vb
@@ -10,7 +10,7 @@ Public Class AutoTag_WinOCR
     Inherits Vblib.AutotaggerBase
     Public Overrides ReadOnly Property Typek As Vblib.AutoTaggerType = Vblib.AutoTaggerType.Local
     Public Overrides ReadOnly Property Nazwa As String = ExifSource.AutoWinOCR ' "AUTO_WINOCR"
-    Public Overrides ReadOnly Property MinWinVersion As String = "10.0"
+    Public Overrides ReadOnly Property MinWinVersion As String = "7.0"
     Public Overrides ReadOnly Property DymekAbout As String = "Próbuje zrobiæ OCR u¿ywaj¹c Windows." & vbCrLf & "U¿ywa pola UserComment"
     Public Overrides ReadOnly Property includeMask As String = "*.jpg;*.jpg.thumb;*.nar;*.png"
 
