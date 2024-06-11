59f59a9fd815976fced7e078fad745e86bf31116
pkar <pkar@ieee.org>
2024-06-11 11:33:22 +0200

    feat: szbysze szukanie w arch.json wg targetdir, filename, gdziekolwiek, geo

--- VbLib/Database/DatabaseJSON.lib.vb
+++ VbLib/Database/DatabaseJSON.lib.vb
@@ -3,6 +3,7 @@
 
 
 Imports System.IO
+Imports pkar.DotNetExtensions
 
 Public Class DatabaseJSON
     Implements DatabaseInterface
@@ -110,16 +111,17 @@ Public Class DatabaseJSON
 
         If Not IsLoaded Then Return Nothing
 
-        ' *TODO* optymalizacja query, przez użycie x.SumKwds.Contains(query.tags), i dopiero na koncu to co zostanie idzie wedlug pelnego szukania - pozniej bedzie dobre dla SQLa
 
         Dim lista As IEnumerable(Of OnePic) = _allItems
+
+        ' najpierw szybkie ograniczanie ilości itemów
 
         ' etap 1 - słowa kluczowe
         If Not String.IsNullOrWhiteSpace(query.ogolne.Tags) Then
             For Each kwd As String In query.ogolne.Tags.Split(" ")
                 If kwd.Substring(0, 1) = "!" Then
                     Dim notkwd As String = kwd.Substring(1)
-                    lista = lista.Where(Function(x) Not If(x?.sumOfKwds.Contains(notkwd), False))
+                    lista = lista.Where(Function(x) Not If(x?.sumOfKwds.Contains(notkwd), True))
                 Else
                     lista = lista.Where(Function(x) If(x?.sumOfKwds.Contains(kwd), False))
                 End If
@@ -128,10 +130,83 @@ Public Class DatabaseJSON
 
         ' etap 2 - wedle serno
         If query.ogolne.serno > 0 Then
+            ' IEnumerable nie ma Clear, ani New, więc nie mogę podmienić tego na List
+            ' Dim onePic As OnePic = lista.FirstOrDefault(Function(x) If(x?.serno, 0) = query.ogolne.serno)
             lista = lista.Where(Function(x) If(x?.serno, 0) = query.ogolne.serno)
         End If
 
+        ' targetDir
+        If Not String.IsNullOrWhiteSpace(query.ogolne.adv.TargetDir) Then
+            If query.ogolne.adv.TargetDir = "!" Then
+                lista = lista.Where(Function(x) String.IsNullOrWhiteSpace(x?.TargetDir))
+            Else
+                For Each kwd As String In query.ogolne.adv.TargetDir.Split(" ")
+                    If kwd.Substring(0, 1) = "!" Then
+                        Dim notkwd As String = kwd.Substring(1)
+                        lista = lista.Where(Function(x) Not If(x?.TargetDir.Contains(notkwd), True))
+                    Else
+                        lista = lista.Where(Function(x) If(x?.TargetDir.Contains(kwd), False))
+                    End If
+                Next
+            End If
+        End If
+
+        ' filename
+        If Not String.IsNullOrWhiteSpace(query.ogolne.adv.Filename) Then
+            lista = lista.Where(Function(x) x.MatchesMasks(query.ogolne.adv.Filename, ""))
+        End If
+
+        ' gdziekolwiek - wersja zgrubna, tak by było szybko - dokładniej i tak będzie w ostatnim etapie szukania
+        If Not String.IsNullOrWhiteSpace(query.ogolne.Gdziekolwiek) Then
+            ' wspóne - tekst w paru miejscach: Descriptions,  Folder, Filename, OCR, Azure description
+
+            ' najpierw wykluczenie - nie może być żadnego z fragmentów
+            For Each kwd As String In query.ogolne.Gdziekolwiek.Split(" ")
+                If Not kwd.Substring(0, 1) = "!" Then Continue For
+                Dim notkwd As String = kwd.Substring(1)
+                lista = lista.Where(Function(x) Not If(x?.TargetDir.ContainsCI(notkwd), True))
+                lista = lista.Where(Function(x) Not If(x?.sSuggestedFilename.ContainsCI(notkwd), True))
+                lista = lista.Where(Function(x) Not If(x?.sumOfDescr?.ContainsCI(notkwd), True))
+                lista = lista.Where(Function(x) Not If(x?.sumOfUserComment?.ContainsCI(notkwd), True))
+            Next
+
+            ' teraz dodanie - musi być każdy fragment, ale gdziekolwiek (albo dowolny fragment gdziekolwiek)
+            For Each kwd As String In query.ogolne.Gdziekolwiek.Split(" ")
+                If kwd.Substring(0, 1) = "!" Then Continue For
+
+                ' ten fragment musi wystąpić - ale może gdziekolwiek wystąpić, więc appendujemy wystąpienia z różnych miejsc
+                Dim newlista As New List(Of OnePic)
+                newlista = lista.Where(Function(x) If(x?.TargetDir.ContainsCI(kwd), True))
+                newlista = newlista.Concat(lista.Where(Function(x) If(x?.sSuggestedFilename.ContainsCI(kwd), True)))
+                newlista = newlista.Concat(lista.Where(Function(x) If(x?.sumOfDescr?.ContainsCI(kwd), True)))
+                newlista = newlista.Concat(lista.Where(Function(x) If(x?.sumOfUserComment?.ContainsCI(kwd), True)))
+
+                lista = newlista
+            Next
+
+        End If
+
+        If query.ogolne?.geo?.Location IsNot Nothing Then
+            ' bardzo zgrubne, jeden stopień ≈ 111 km (dla 0,0), czyli 0.01 stopnia to nie więcej niż 1.1 km
+            'lista = lista.Where(Function(x)
+            '                        If x?.sumOfGeo Is Nothing Then Return False
+            '                        If (x.sumOfGeo.Latitude - query.ogolne.geo.Location.Latitude).Abs > 0.01 Then Return False
+            '                        If (x.sumOfGeo.Longitude - query.ogolne.geo.Location.Longitude).Abs > 0.01 Then Return False
+            '                        Return True
+            '                    End Function)
+
+            Dim minLat As Double = query.ogolne.geo.Location.Latitude - 0.01
+            Dim maxLat As Double = query.ogolne.geo.Location.Latitude + 0.01
+            Dim minLon As Double = query.ogolne.geo.Location.Longitude - 0.01
+            Dim maxLon As Double = query.ogolne.geo.Location.Longitude + 0.01
+
+            lista = lista.Where(Function(x) If(x?.sumOfGeo?.Latitude, -500) > minLat AndAlso If(x?.sumOfGeo?.Latitude, -500) < maxLat AndAlso If(x?.sumOfGeo?.Longitude, -500) > minLon AndAlso If(x?.sumOfGeo?.Longitude, -500) < maxLon)
+
+        End If
+
         ' etap LAST - dalsze szukanie
+        ' to co zostanie po szybkim wyszukaniu idzie według pełnego szukania - pozniej bedzie dobre dla SQLa
+
         ' dziwne, ale 5 razy takie wyszło (null) - dwa przecinki pod rząd, pewnie przy dodawaniu do archiwumm
         Return lista.Where(Function(x) If(x?.CheckIfMatchesQuery(query), False))
 
