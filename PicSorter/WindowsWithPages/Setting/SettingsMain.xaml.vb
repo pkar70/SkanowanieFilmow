20fdac9cbcf25a122459a0c545caf7964d9591cd
pkar <pkar@ieee.org>
2024-08-05 12:24:51 +0200

    feat: reorganizacja settings (wydzielone rzadkie)

--- PicSorter/WindowsWithPages/Setting/SettingsMain.xaml.vb
+++ PicSorter/WindowsWithPages/Setting/SettingsMain.xaml.vb
@@ -12,12 +12,9 @@ Class SettingsMain
 
         uiVersion.ShowAppVers(True)
         If vb14.GetSettingsString("uiFolderBuffer") = "" Then
-            uiGlobalSett_Click(Nothing, Nothing)
+            Me.NavigationService.Navigate(New SettingsGlobal)
+            'uiGlobalSett_Click(Nothing, Nothing)
         End If
-    End Sub
-
-    Private Sub uiGlobalSett_Click(sender As Object, e As RoutedEventArgs)
-        Me.NavigationService.Navigate(New SettingsGlobal)
     End Sub
 
     Private Sub uiSettSources_Click(sender As Object, e As RoutedEventArgs)
@@ -38,25 +35,7 @@ Class SettingsMain
 
 
 
-    Private Sub uiDirTree_Click(sender As Object, e As RoutedEventArgs)
-        Dim oWnd As New SettingsDirTree(True)
-        oWnd.ShowDialog()
-    End Sub
 
-    Private Sub uiKeywords_Click(sender As Object, e As RoutedEventArgs)
-        Dim oWnd As New SettingsKeywords
-        oWnd.ShowDialog()
-        ' Me.NavigationService.Navigate(New SettingsKeywords)
-    End Sub
-
-    Private Sub uiAutoTags_Click(sender As Object, e As RoutedEventArgs)
-        Me.NavigationService.Navigate(New SettingsAutoTags)
-    End Sub
-
-
-    Private Sub uiListSett_Click(sender As Object, e As RoutedEventArgs)
-        Me.NavigationService.Navigate(New SettingListy)
-    End Sub
     Private Sub uiMiscSett_Click(sender As Object, e As RoutedEventArgs)
         Me.NavigationService.Navigate(New SettingsMisc)
     End Sub
@@ -67,7 +46,7 @@ Class SettingsMain
         oPicker.Title = "Select directory for backup"
         oPicker.FileName = "picsorter." & Date.Now.ToString("yyyyMMdd") & ".zip"
         oPicker.CheckPathExists = True
-        oPicker.InitialDirectory = Application.GetDataFolder
+        oPicker.InitialDirectory = vblib.GetDataFolder
 
         ' Show open file dialog box
         Dim result? As Boolean = oPicker.ShowDialog()
@@ -95,10 +74,10 @@ Class SettingsMain
 
         Await Task.Run(Sub()
                            Dim oArchive = IO.Compression.ZipFile.Open(filename, IO.Compression.ZipArchiveMode.Create)
-                           For Each sFile As String In IO.Directory.GetFiles(Application.GetDataFolder, "*.txt")
+                           For Each sFile As String In IO.Directory.GetFiles(vblib.GetDataFolder, "*.txt")
                                oArchive.CreateEntryFromFile(sFile, IO.Path.GetFileName(sFile))
                            Next
-                           For Each sFile As String In IO.Directory.GetFiles(Application.GetDataFolder, "*.json")
+                           For Each sFile As String In IO.Directory.GetFiles(vblib.GetDataFolder, "*.json")
                                oArchive.CreateEntryFromFile(sFile, IO.Path.GetFileName(sFile))
                            Next
 
@@ -119,5 +98,9 @@ Class SettingsMain
 
     Private Sub uiSharingSett_Click(sender As Object, e As RoutedEventArgs)
         Me.NavigationService.Navigate(New SettingsShare)
+    End Sub
+
+    Private Sub uiSharingRzadie_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingsRare)
     End Sub
 End Class
---
+++ PicSorter/WindowsWithPages/Setting/SettingsRare.xaml.vb
@@ -0,0 +1,37 @@
+﻿
+'Imports vb14 = Vblib.pkarlibmodule14
+Imports pkar.UI.Configs.Extensions
+Imports pkar.UI.Extensions
+
+Class SettingsRare
+
+    Private Sub uiDirTree_Click(sender As Object, e As RoutedEventArgs)
+        Dim oWnd As New SettingsDirTree(True)
+        oWnd.ShowDialog()
+    End Sub
+
+    Private Sub uiKeywords_Click(sender As Object, e As RoutedEventArgs)
+        Dim oWnd As New SettingsKeywords
+        oWnd.ShowDialog()
+        ' Me.NavigationService.Navigate(New SettingsKeywords)
+    End Sub
+
+    Private Sub uiListSett_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingListy)
+    End Sub
+    Private Sub uiGlobalSett_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingsGlobal)
+    End Sub
+
+    Private Sub uiWebAutoTags_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingsWebAutoTags)
+    End Sub
+
+    Private Sub uiAutoTagsDefs_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingsAutoTagsDef)
+    End Sub
+
+    Private Sub uiSequenceSett_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingsSequence)
+    End Sub
+End Class
