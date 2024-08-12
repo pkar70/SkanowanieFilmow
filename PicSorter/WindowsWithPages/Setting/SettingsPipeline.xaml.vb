2ceebae26966fc46ce134fdb8c562bca8ce08d6b
pkar <pkar@ieee.org>
2024-08-12 19:57:38 +0200

    feat: domyslne signature (gdy nie ma w EXIFach)

---
+++ PicSorter/WindowsWithPages/Setting/SettingsPipeline.xaml.vb
@@ -0,0 +1,20 @@
+﻿Imports pkar.UI.Configs.Extensions
+Imports pkar.UI.Extensions
+
+
+Class SettingsPipeline
+    Private Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+        uiWinFaceMaxAge.GetSettingsInt()
+        uiWinFaceMinSize.GetSettingsInt()
+    End Sub
+
+    Private Sub uiOK_Click(sender As Object, e As RoutedEventArgs)
+        uiWinFaceMaxAge.SetSettingsInt()
+        uiWinFaceMinSize.SetSettingsInt()
+    End Sub
+
+    Private Sub uiWatermark_Click(sender As Object, e As RoutedEventArgs)
+        Me.NavigationService.Navigate(New SettingsWatermark)
+    End Sub
+
+End Class
--- Process_n6_SigAndFace/Process_Signature.vb
+++ Process_n6_SigAndFace/Process_Signature.vb
@@ -200,69 +200,7 @@ Public Class Process_Signature
 
         Return Await Process_EmbedTexts.ApplyMainShared(oPic, bPipeline, "Br|20|0.05|50|" & signature)
 
-        '        oPic.InitEdit(bPipeline)
-
-        '        ' Dim watermarkedStream As IO.Stream = IO.File.OpenWrite(oPic.sFilenameEditDst)
-
-        '        Dim signature As String = params
-        '        If String.IsNullOrWhiteSpace(signature) Then signature = GetSignatureString(oPic)
-
-        '        ' Using oStream As IO.Stream = IO.File.OpenRead(oPic.sFilenameEditSrc)
-
-        '        Using img = Image.FromStream(oPic._PipelineInput)   ' oStream
-
-        '            Dim fontSize As Integer = 20        ' minimalna wielkoœæ
-        '            fontSize = Math.Max(fontSize, img.Height * 0.05)    ' 5 % obrazka
-        '            fontSize = Math.Min(fontSize, 50)       ' ale nie wiêcej ni¿ 50 px
-
-        '            Using graphic = Graphics.FromImage(img)
-        '                Dim oFont As New Font(FontFamily.GenericSansSerif, fontSize, FontStyle.Bold, GraphicsUnit.Pixel)
-
-        '                graphic.PageUnit = GraphicsUnit.Pixel
-        '                Dim oMeasure As SizeF = graphic.MeasureString(signature, oFont)
-
-        '                Dim iWhereX As Integer = img.Width - oMeasure.Width
-        '                iWhereX = Math.Max(iWhereX, 10)
-
-
-        '                Dim oBrush As New SolidBrush(Color.Gray)
-        '                Dim oPoint As New Point(iWhereX, img.Height - fontSize * 2)
-
-        '                graphic.DrawString(signature, oFont, oBrush, oPoint)
-        '#If STARA_WERSJA Then
-        '                img.Save(oPic._PipelineOutput, Imaging.ImageFormat.Jpeg)    '   watermarkedStream
-        '#Else
-        '                img.Save(oPic._PipelineOutput, GetEncoder(ImageFormat.Jpeg), GetJpgQuality)
-        '#End If
-
-
-        '            End Using
-        '        End Using
-        '        ' End Using
-
-
-        '        oPic.EndEdit(True, False)
-
-        '        Return True
     End Function
-
-    'Public Shared Function GetJpgQuality(Optional iQuality As Integer = 0) As EncoderParameters
-    '    If iQuality = 0 Then iQuality = vb14.GetSettingsInt("uiJpgQuality")
-
-    '    ' https://stackoverflow.com/questions/1484759/quality-of-a-saved-jpg-in-c-sharp
-    '    Dim myEncoder As Imaging.Encoder = Imaging.Encoder.Quality
-    '    Dim myEncoderParameters As New EncoderParameters(1)
-    '    myEncoderParameters.Param(0) = New EncoderParameter(myEncoder, iQuality)
-    '    Return myEncoderParameters
-    'End Function
-
-    'Public Shared Function GetEncoder(format As ImageFormat) As ImageCodecInfo
-
-    '    For Each codec As ImageCodecInfo In ImageCodecInfo.GetImageDecoders()
-    '        If codec.FormatID = format.Guid Then Return codec
-    '    Next
-    '    Return Nothing
-    'End Function
 
     ''' <summary>
     ''' zwraca Copyright po FlattenExifs
@@ -270,17 +208,9 @@ Public Class Process_Signature
     Private Function GetSignatureString(oPic As Vblib.OnePic) As String
 
         Dim oExif As Vblib.ExifTag = oPic.FlattenExifs(False)
-        Return oExif.Copyright
-        'Dim sSignature As String = ""
+        If Not String.IsNullOrWhiteSpace(oExif.Copyright) Then Return oExif.Copyright
+        Return Vblib.GetSettingsString("uiDefaultCopyr")
 
-        'For Each oExif As Vblib.ExifTag In oPic.Exifs
-        '    If Not String.IsNullOrWhiteSpace(oExif.Copyright) Then sSignature = oExif.Copyright
-        'Next
-
-        'Dim iInd As Integer = sSignature.IndexOfAny({".", ","})
-        'If iInd > 1 Then sSignature = sSignature.Substring(0, iInd)
-
-        'Return sSignature
     End Function
 
 
