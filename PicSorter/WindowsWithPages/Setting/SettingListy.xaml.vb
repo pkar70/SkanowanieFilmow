578f40a6a17beef524d2983f832e3ea11442bff3
pkar <pkar@ieee.org>
2024-08-23 10:55:46 +0200

    feat: lista execow

--- PicSorter/WindowsWithPages/Setting/SettingListy.xaml.vb
+++ PicSorter/WindowsWithPages/Setting/SettingListy.xaml.vb
@@ -1,17 +1,17 @@
 ﻿
 Class SettingListy
     Private Sub uiListCopyr_Click(sender As Object, e As RoutedEventArgs)
-        Dim oWnd As New EditEntryHist(Vblib.GetDataFolder, "Copyrights", "Dodaj właściciela praw", "(c) KTO. All rights reserved.")
+        Dim oWnd As New EditEntryHist("Copyrights", "Dodaj właściciela praw", "(c) KTO. All rights reserved.")
         oWnd.Show()
     End Sub
 
     Private Sub uiListAuthor_Click(sender As Object, e As RoutedEventArgs)
-        Dim oWnd As New EditEntryHist(Vblib.GetDataFolder, "Authors", "Dodaj autora (zwykle: imię nazwisko)", "")
+        Dim oWnd As New EditEntryHist("Authors", "Dodaj autora (zwykle: imię nazwisko)", "")
         oWnd.Show()
     End Sub
 
     Private Sub uiListInetAuthor_Click(sender As Object, e As RoutedEventArgs)
-        Dim oWnd As New EditEntryHist(Vblib.GetDataFolder, "inetauthors", "Dodaj autora (zwykle: imię nazwisko) zdjęć z Internet", "")
+        Dim oWnd As New EditEntryHist("inetauthors", "Dodaj autora (zwykle: imię nazwisko) zdjęć z Internet", "")
         oWnd.Show()
     End Sub
     'Private Sub uiListCameraMakers_Click(sender As Object, e As RoutedEventArgs)
@@ -19,12 +19,20 @@ Class SettingListy
     '    oWnd.Show()
     'End Sub
     Private Sub uiListCameraModels_Click(sender As Object, e As RoutedEventArgs)
-        Dim oWnd As New EditEntryHist(Vblib.GetDataFolder, "Cameras", "Dodaj model aparatu/skanera (lub producent # model)", "")
+        Dim oWnd As New EditEntryHist("Cameras", "Dodaj model aparatu/skanera (lub producent # model)", "")
         oWnd.Show()
     End Sub
+
+    ' shared, bo także z user context menu tychże
+    Public Shared Sub uiListExec_Click(sender As Object, e As RoutedEventArgs)
+        Dim oWnd As New EditEntryHist("Execs", "Dodaj executable do edycji/podglądu zdjęcia, użyj %f jako pic placeholder. Pamiętaj o cudzysłowach!", "")
+        oWnd.Show()
+    End Sub
+
 
     Private Sub uiSettMaps_Click(sender As Object, e As RoutedEventArgs)
         Me.NavigationService.Navigate(New SettingsMapsy)
     End Sub
+
 
 End Class
